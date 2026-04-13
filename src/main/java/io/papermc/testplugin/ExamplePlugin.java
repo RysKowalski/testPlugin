bf95b4395b75643d047c35298951b4ad9bb91afa
RysKowalski <ryskowalski0@gmail.com>
2026-04-13 13:19:34 +0200

    plugin

---
+++ src/main/java/io/papermc/testplugin/ExamplePlugin.java
@@ -0,0 +1,60 @@
+package io.papermc.testplugin;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.joml.Math;
+
+import io.papermc.testplugin.commands.HelloCommand;
+
+// all players have 9 saved accessories that are items
+//
+// function updateAglet()
+// checks if saved accessories have oak plank named "Aglet"
+// if yes: set generic movement speed of player to 0.12
+// else: reset generic movement speed
+//
+// command /aglet
+// gives player oak plank named "Aglet"
+//
+// command /akcesoria
+// opens chest ui with single row, named "Akcesoria"
+// user can get accesory from own inventory, and drag it to this gui
+// if item is not accesory, at this moment oak plank named "Aglet"
+// sends message "To nie jest akcesorium"
+// on close of this menu:
+// saves items in gui to user's saved accessories
+//
+// on join: updateAglet()
+
+public class ExamplePlugin extends JavaPlugin implements Listener {
+  @Override
+  public void onEnable() {
+    Bukkit.getPluginManager().registerEvents(this, this);
+    getCommand("hello").setExecutor(new HelloCommand());
+  }
+
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent event) {
+    event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    PotionEffect effect = new PotionEffect(
+        PotionEffectType.SPEED,
+        200,
+        1);
+    event.getPlayer().addPotionEffect(effect);
+  }
+
+  @EventHandler
+  public void onPlayerMove(PlayerMoveEvent event) {
+    if (Math.random() > 0.95) {
+      event.getPlayer().sendMessage(Component.text("masz skill issue naucz się chodzić"));
+      event.setCancelled(true);
+    }
+  }
+}
